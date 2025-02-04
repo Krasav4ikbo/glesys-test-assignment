6a2da1620d303645753900c6353c391136e76ec9
Pavel Bosy <bosyypavel@gmail.com>
2025-02-04 09:53:29 +0100

    Add migration for heartbeat table and seeds for it

--- api/database/seeders/DefaultDatabaseSeeder.php
+++ api/database/seeders/DefaultDatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Heartbeat;
 use Butler\Service\Models\Consumer;
 use Illuminate\Database\Seeder;
 
@@ -9,8 +10,9 @@ class DefaultDatabaseSeeder extends Seeder
 {
     public function run()
     {
-        if (app()->isLocal()) {
+        if (app()->isLocal() || app()->runningUnitTests()) {
             $this->seedDeveloperConsumer();
+            $this->seedHeartBeat();
         }
     }
 
@@ -22,5 +24,19 @@ class DefaultDatabaseSeeder extends Seeder
                 'token' => hash('sha256', 'secret'),
                 'abilities' => ['*'],
             ]);
+    }
+
+    private function seedHeartBeat(): void
+    {
+        for ($i = 0; $i < 10; $i++)
+        {
+            Heartbeat::create([
+                'applicationKey' => 'app-' . $i,
+                'heartbeatKey' => 'source-' . $i,
+                'unhealthyAfterMinutes' => $i,
+                'updated_at' => date('Y-m-d H:i:s'),
+            ]);
+        }
+
     }
 }
